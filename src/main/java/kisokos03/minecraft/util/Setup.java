768a4f23de5cc92b72e1d252af5b516bddacef02
kisokos03 <bagimate@gmail.com>
2022-03-09 03:09:05 +0000

    more streamlined setup, allows for forced reinstall

--- src/main/java/kisokos03/minecraft/util/Setup.java
+++ src/main/java/kisokos03/minecraft/util/Setup.java
@@ -1,20 +1,20 @@
 package kisokos03.minecraft.util;
 
-import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.Arrays;
 import java.util.List;
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.io.File;
 import java.io.IOException;
-import java.util.List;
 import java.util.stream.Collectors;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.InvalidRemoteException;
+import org.eclipse.jgit.api.errors.TransportException;
 import org.bukkit.Bukkit;
 
 public class Setup {
-    public Boolean firstTimeSetupCheck() {
+    private Boolean firstTimeSetupCheck() {
         boolean setup = false;
         try {
             String webhooksDir = ".\\plugins\\webhooks";
@@ -44,9 +44,33 @@ public class Setup {
 
     }
 
-    // private void firstTimeSetup() {
-    //     // download js files
-    //     // test node, throw error if not present
+    public void nodeAppSetup(Boolean forced) {
 
-    // }
+        // download js files
+        // test node, throw error if not present
+        Setup Setup = new Setup();
+
+        if ((!Setup.firstTimeSetupCheck()) || forced) {
+            
+            try {
+                Files.walk(Path.of("./plugins/webhooks"));
+            } catch (IOException e) {
+                Bukkit.getLogger().info(e.toString());
+            }
+
+            try {
+                Git.cloneRepository()
+                        .setURI("https://github.com/kisokos03/MCWebhooksNode")
+                        .setDirectory(new File("./plugins/webhooks"))
+                        .call();
+            } catch (InvalidRemoteException e) {
+                Bukkit.getLogger().info(e.toString());
+            } catch (TransportException e) {
+                Bukkit.getLogger().info(e.toString());
+            } catch (GitAPIException e) {
+                Bukkit.getLogger().info(e.toString());
+            }
+
+        }
+    }
 }
