9c98a06eb4e2b83c6387924feca419d4f6c50b05
kisokos03 <bagimate@gmail.com>
2022-03-09 00:35:01 +0000

    added some setup functionality

--- src/main/java/kisokos03/minecraft/Webhooks.java
+++ src/main/java/kisokos03/minecraft/Webhooks.java
@@ -1,8 +1,8 @@
 package kisokos03.minecraft;
 
-import kisokos03.minecraft.NodeCommands;
 import kisokos03.minecraft.commands.*;
 import kisokos03.minecraft.listener.*;
+import kisokos03.minecraft.util.*;
 
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.RegisteredListener;
@@ -11,35 +11,52 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.server.ServerListPingEvent;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandExecutor;
 import com.destroystokyo.paper.event.server.PaperServerListPingEvent;
-
-import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
-
-public class Webhooks extends JavaPlugin{
+public class Webhooks extends JavaPlugin {
 
     NodeCommands Node = new NodeCommands();
-  
+    Setup Setup = new Setup();
 
-    @Override
-    public void onEnable(){
-        Node.sendWebhook("enable");
-
-        this.getCommand("initializeWebhook").setExecutor(new WebhookInitializerCommand());
-        this.getCommand("checkOnlinePlayers").setExecutor(new CheckOnlinePlayersCommand());
-        getLogger().info("added initializer command");
-
-        getServer().getPluginManager().registerEvents(new PlayerQuitListener(), this);
-        getServer().getPluginManager().registerEvents(new PlayerJoinListener(), this);
-        
+    public static void downloadFile(URL url, String fileName) throws Exception {
+        try (InputStream in = url.openStream()) {
+            Files.copy(in, Paths.get(fileName));
+        }
     }
-    
+
     @Override
-    public void onDisable(){
+    public void onEnable() {
+        Bukkit.getLogger().info("setup check: " + Boolean.toString(Setup.firstTimeSetupCheck()));
+        try {
+            downloadFile(new URL("https://github.com/kisokos03/MCWebhooks/blob/main/nodeapp/wh.js"), "WEBHOOKDOWNLOAD.js");
+        } catch (Exception e) {
+
+        }
+
+        // Node.sendWebhook("enable");
+        // this.getCommand("initializeWebhook").setExecutor(new
+        // WebhookInitializerCommand());
+        // this.getCommand("checkOnlinePlayers").setExecutor(new
+        // CheckOnlinePlayersCommand());
+        // getLogger().info("added initializer command");
+
+        // getServer().getPluginManager().registerEvents(new PlayerQuitListener(),
+        // this);
+        // getServer().getPluginManager().registerEvents(new PlayerJoinListener(),
+        // this);
+
+    }
+
+    @Override
+    public void onDisable() {
         Node.sendWebhook("disable");
 
     }
 }
-    
